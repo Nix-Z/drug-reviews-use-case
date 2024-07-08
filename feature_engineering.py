b3af85573e0cb2e84d89c7fda21f3fccc36f1b56
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2024-07-07 18:18:32 -0700

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from datavisualization import visualize_data
 
 # Download VADER lexicon
-nltk.download('vader_lexicon')
+# nltk.download('vader_lexicon')
 
 def engineer_features():
     data = visualize_data()
@@ -27,7 +27,7 @@ def engineer_features():
     data['comments_sentiment'] = data['commentsReview'].apply(get_sentiment_scores)
     
     # Drop rows that were used for sentiment analysis
-    data.drop(['urlDrugName', 'benefitsReview', 'sideEffectsReview', 'commentsReview'], axis=1, inplace=True)
+    data.drop(['benefitsReview', 'sideEffectsReview', 'commentsReview', 'urlDrugName'], axis=1, inplace=True)
 
     # Convert categorical columns to numerical
     data.replace({'effectiveness': {
@@ -51,19 +51,7 @@ def engineer_features():
     '''
     le = LabelEncoder()
     data['urlDrugName'] = le.fit_transform(data['urlDrugName'])
-
-    # Scale the Data
-    new_data = data.drop(columns=['rating'])
-    scaler = StandardScaler()
-    scaler.fit(new_data)
-    scaled_features = scaler.transform(new_data)
-    scaled_data = pd.DataFrame(scaled_features,columns=new_data.columns[:])
-    selected_columns = data[['rating']]
-    scaled_data[['rating']] = selected_columns.copy()
-    data = scaled_data
     '''
-
-    data.dropna(axis=0, how='any', inplace=True) # Drop any rows with null values
     
     print(data.head())
     
